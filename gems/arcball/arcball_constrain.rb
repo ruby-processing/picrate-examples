46f6111a07ce0029e77eb336ff97e54543d3436b
monkstone <mamba2928@yahoo.co.uk>
2018-05-06 19:54:18 +0100

    add gem examples

---
+++ gems/arcball/arcball_constrain.rb
@@ -0,0 +1,32 @@
+#!/usr/bin//env jruby -v -w
+
+require 'picrate'
+require 'arcball'
+
+############################
+# Use mouse drag to rotate
+# the arcball. Use mousewheel
+# to zoom. Custom constrain
+# default is yaxis.
+############################
+
+class ArcballBox < Processing::App
+
+  def settings
+    size(600, 600, P3D)
+    smooth(8)
+  end
+
+  def setup
+    sketch_title 'ArcBall Box'
+    Processing::ArcBall.constrain(self, :xaxis)
+    fill 180
+  end
+
+  def draw
+    background(50)
+    box(300, 300, 300)
+  end
+end
+
+ArcballBox.new
---
+++ gems/geomerative/hello_svg_to_pdf.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env jruby -w
+require 'picrate'
+require 'geomerative'
+
+class HelloSvgToPDF < Processing::App
+  load_library 'pdf'
+  attr_reader :grp, :pdf
+
+  def settings
+    size(400, 400)
+  end
+
+  def setup
+    sketch_title 'SVG to PDF sketch'
+    RG.init(self)
+    @grp = RG.load_shape(data_path('bot1.svg'))
+    @pdf = create_graphics(width, height, PDF, 'bot1.pdf')
+  end
+
+  def draw
+    background(255)
+    grp.draw
+    pdf.begin_draw
+    pdf.background(255)
+    grp.draw(pdf)
+    pdf.dispose
+    pdf.end_draw
+  end
+end
+
+HelloSvgToPDF.new
