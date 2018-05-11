e2df9bba567b1a0ab194f7bc5c7a7ec7d8ab38f2
monkstone <mamba2928@yahoo.co.uk>
2018-05-11 15:38:37 +0100

    more examples

---
+++ shaders/infinite_tiling.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env jruby
+require 'picrate'
+#-------------------------------------------------------------
+# Display endless moving background using a tile texture.
+# Contributed by martiSteiger
+#-------------------------------------------------------------
+class InfiniteTiling < Processing::App
+  attr_reader :tile_texture, :tile_shader, :time0
+
+  def settings
+    size(640, 480, P2D)
+  end
+
+  def setup
+    sketch_title 'Infinite Tiling'
+    texture_wrap(REPEAT)
+    @tile_texture = load_image(data_path('penrose.jpg'))
+    load_tile_shader
+    @time0 = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  end
+
+  def load_tile_shader
+    @tile_shader = load_shader(data_path('scroller.glsl'))
+    tile_shader.set('resolution', width.to_f, height.to_f)
+    tile_shader.set('tileImage', tile_texture)
+  end
+
+  def draw
+    mil = Process.clock_gettime(Process::CLOCK_MONOTONIC) - time0
+    tile_shader.set('time', mil)
+    shader(tile_shader)
+    rect(0, 0, width, height)
+  end
+end
+
+InfiniteTiling.new
---
+++ shaders/landscape.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env jruby -v -W2
+# frozen_string_literal: true
+require 'picrate'
+
+class Landscape < Processing::App
+  #
+  # Elevated
+  # https://www.shadertoy.com/view/MdX3Rr by inigo quilez
+  # Created by inigo quilez - iq/2013
+  # License Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License.
+  # Processing port by Raphaël de Courville.
+  #
+  attr_reader :landscape
+  java_alias :background_int, :background, [Java::int]  
+  TITLE_FORMAT = 'frame: %d - fps: %0.2f'.freeze
+
+  def settings
+    size(640, 360, P2D)
+  end
+
+  def setup
+    sketch_title 'Landscape'
+    no_stroke
+    # The code of this shader shows how to integrate shaders from shadertoy
+    # into Processing/JRubyArt/propane with minimal changes.
+    @landscape = load_shader(data_path('landscape.glsl'))
+    landscape.set('resolution', width.to_f, height.to_f)
+  end
+
+  def draw
+    background_int 0
+    landscape.set('time', (millis/1000.0).to_f)
+    shader(landscape)
+    rect(0, 0, width, height)
+    sketch_title(format(TITLE_FORMAT, frame_count, frame_rate))
+  end
+end
+
+Landscape.new
