93a7fb933f6e672024b38db9f0dae5f83b5ca182
Martin Prout <mamba2928@yahoo.co.uk>
2019-10-20 06:47:18 +0100

    Mention geany in README

--- basics/typography/font_list.rb
+++ basics/typography/font_list.rb
@@ -13,7 +13,7 @@ class FontList < Processing::App
   def setup
     # Uncomment the following two lines to see the available fonts
     Java::ProcessingCore::PFont.list.each { |fot| puts fot }
-    my_font = create_font('Noto Sans', 32)
+    my_font = create_font('DejaVu Sans', 32)
     text_font(my_font)
     text_align(CENTER, CENTER)
     text('!@#$%', width / 2, height / 2)
