e029122c9ab12c134e6db8f3ed31077a15eb578c
monkstone <mamba2928@yahoo.co.uk>
2021-05-14 15:40:43 +0100

    re-organise code

--- external_library/java/pgs/contour_map.rb
+++ external_library/java/pgs/contour_map.rb
@@ -1,56 +1,61 @@
-#!/usr/bin/env jruby --dev
+#!/usr/bin/env jruby
+
 require 'picrate'
-require_relative './vector_list'
+
 class ContourMap < Processing::App
-  load_library :pgs
-  java_import 'micycle.pgs.PGS_Contour'
+load_libraries :pgs, :vector_list
+java_import 'micycle.pgs.PGS_Contour'
 
-  attr_reader :heights, :max, :min
-  MAX = -1
-  MIN = 9999
+attr_reader :polygon, :heights, :max, :min
+MAX = -1
+MIN = 9999
 
-  def setup
-    sketch_title 'Contour Map'
-    @max = MAX
-    @min = MIN
-  end
+def setup
+  sketch_title 'Contour Map'
+  @max = MAX
+  @min = MIN
+end
 
-  def draw
-    background(0, 0, 40)
-    populate_height_map
-    isolines = PGS_Contour.isolines(heights.array_list, 0.08, min, max)
-    isolines.to_hash.each do |isoline, value|
-      isoline.set_stroke(
-        color(
-          map1d(value, min..max, 50..255),
-          map1d(isoline.get_vertex(0).x, 0..width, 50..255),
-          map1d(isoline.get_vertex(0).y, 0..height, 50..255)
-        )
+def draw
+  background(0, 0, 40)
+  populate_height_map
+  isolines = PGS_Contour.isolines(heights.array_list, 0.08, min, max)
+  isolines.to_hash.each do |isoline, value|
+    isoline.set_stroke(
+      color(
+        map1d(value, min..max, 50..255),
+        map1d(isoline.get_vertex(0).x, 0..width, 50..255),
+        map1d(isoline.get_vertex(0).y, 0..height, 50..255)
       )
-      shape(isoline)
-    end
+    )
+    shape(isoline)
   end
+end
 
-  def populate_height_map
-    @heights = VectorList.new
+def populate_height_map
+  @heights = VectorList.new
 
-    res = 16
-    anim_speed = 0.005
+  res = 16
+  anim_speed = 0.005
 
-    grid(width + res, height + res, res, res) do |x, y|
-      z = norm(noise(x*0.01 + frame_count*anim_speed, y*0.01 + frame_count*anim_speed), -1.0, 1.0)
-      h = Vec3D.new(x, y, 0)
-      z += h.dist(Vec3D.new(mouse_x, mouse_y, 0))*0.005
-      h.z = z
-      heights << h
-      @max = [max, z].max
-      @min = [min, z].min
-    end
+  grid(width + res, height + res, res, res) do |x, y|
+    z = norm(SmoothNoise.noise(x*0.01 + frame_count*anim_speed, y*0.01 + frame_count*anim_speed), -1.0, 1.0)
+    h = Vec3D.new(x, y, 0)
+    z += h.dist(Vec3D.new(mouse_x, mouse_y, 0))*0.005
+    h.z = z
+    heights << h
+    @max = [max, z].max
+    @min = [min, z].min
   end
-
-  def settings
-    size(800, 800)
+  if frame_count > 20
+    save_frame(data_path('sk0####.tif')) if (5..100).include? frame_count
   end
+  stop if frame_count > 120
+end
+
+def settings
+  size(800, 800, P2D)
+end
 end
 
 ContourMap.new
